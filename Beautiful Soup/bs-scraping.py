d81b9cafc247451a5e65b2bc8dce103234ccffa4
Tatan Vincent <vintatan@visa.com>
2019-06-16 13:25:16 +0800

    Beautiful Soup and Web Scraping with e-Commerce

--- Beautiful Soup/bs-scraping.py
+++ Beautiful Soup/bs-scraping.py
@@ -1,10 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
-url = "https://www.google.com"
-# Add header
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
-r = requests.get(url, headers=headers)
-print(r) # print request to see if Response 200
-soup = BeautifulSoup(r.content, "html.parser")
+import pandas as pd
 
-print(soup)
+# Add header and  url
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
+# url = "https://en.wikipedia.org/wiki/List_of_national_capitals"
+url = "https://www.amazon.com/"
+r = requests.get(url)
+
+# Initiate beautiful and list element to extract all the rows in the table
+soup = BeautifulSoup(r.content, "html.parser")
+table = soup.find_all('table')[1]
+print("table is ", table)
+rows = table.find_all('tr')
+row_list = list()
+
+# Iterate through all of the rows in table and get through each of the cell to append it into rows and row_list
+for tr in rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td]
+    row_list.append(row)
+    print("Row is ", row)
+
+# Create Pandas Dataframe and print it
+df_bs = pd.DataFrame(row_list,columns=['City','Country','Notes'])
+df_bs.set_index('Country',inplace=True)
+print(df_bs.head())
+
+# Exporting the data into csv
+df_bs.to_csv('beautifulsoup.csv')
+
--- Selenium Web Scraping/scraping.py
+++ Selenium Web Scraping/scraping.py
@@ -6,9 +6,9 @@ from selenium.common.exceptions import TimeoutException
 
 import pandas as pd
 
-option = webdriver.ChromeOptions()
 
 # Run the argument with incognito
+option = webdriver.ChromeOptions()
 option.add_argument(' — incognito')
 driver = webdriver.Chrome(executable_path='chromedriver', chrome_options=option)
 
@@ -23,21 +23,21 @@ except TimeoutException:
     driver.quit()
 
 
-# find_elements_by_xpath returns an array of selenium objects.
+# find_elements by ID returns an array of selenium objects.
 category_element = driver.find_element(By.ID,'Level_1_Category_No1').text;
 print("category element ",category_element)
 
 # Take the list of li in the ul
-# list_category_elements = driver.find_element(By.XPATH,'//*[@id="J_icms-5000498-1511516689962"]/div/ul')
-# links = list_category_elements.find_elements(By.CLASS_NAME,"lzd-site-menu-root-item")
-# print('length of links are: ', len(links))
-# for i in range(len(links)):
-#     print("element in list ",links[i].text)
+list_category_elements = driver.find_element(By.XPATH,'//*[@id="J_icms-5000498-1511516689962"]/div/ul')
+links = list_category_elements.find_elements(By.CLASS_NAME,"lzd-site-menu-root-item")
+print('length of links are: ', len(links))
+for i in range(len(links)):
+    print("element in list ",links[i].text)
 
 # Clicking toys menu to find the right one
 # You might receive error if you just perform element.click(). This is due to the element might not actionable according to DOM
-# element = driver.find_elements_by_class_name('J_ChannelsLink')[1]
-# webdriver.ActionChains(driver).move_to_element(element).click(element).perform()
+element = driver.find_elements_by_class_name('J_ChannelsLink')[1]
+webdriver.ActionChains(driver).move_to_element(element).click(element).perform()
 
 # try:
 #     WebDriverWait(driver, timeout).until(EC.visibility_of_element_located((By.CLASS_NAME, "title_wrapper")))
@@ -46,28 +46,28 @@ print("category element ",category_element)
 #     driver.quit()
 
 # Once we are in, let us extract all of the product elements then names
-# product_titles = driver.find_elements_by_class_name('title')
-# for title in product_titles:
-#     print(title.text)
+product_titles = driver.find_elements_by_class_name('title')
+for title in product_titles:
+    print(title.text)
 
 # Let us make a panda dataframe of title, price
-# product_containers = driver.find_elements_by_class_name('product_container')
-#
-# product_titles = list()
-# pack_sizes = list()
-# product_prices = list()
-# rating_counts = list()
-#
-# for container in product_containers:
-#     product_titles.append(container.find_element_by_class_name('title').text)
-#     pack_sizes.append(container.find_element_by_class_name('pack_size').text)
-#     product_prices.append(container.find_element_by_class_name('product_price').text)
-#     rating_counts.append(container.find_element_by_class_name('ratings_count').text)
-#
-# data = {'product_title': product_titles, 'pack_size': pack_sizes,'product_price': product_prices, 'rating_count': rating_counts}
-# df_product = pd.DataFrame.from_dict(data)
-#
-# print(df_product.head())
+product_containers = driver.find_elements_by_class_name('product_container')
+
+product_titles = list()
+pack_sizes = list()
+product_prices = list()
+rating_counts = list()
+
+for container in product_containers:
+    product_titles.append(container.find_element_by_class_name('title').text)
+    pack_sizes.append(container.find_element_by_class_name('pack_size').text)
+    product_prices.append(container.find_element_by_class_name('product_price').text)
+    rating_counts.append(container.find_element_by_class_name('ratings_count').text)
+
+data = {'product_title': product_titles, 'pack_size': pack_sizes,'product_price': product_prices, 'rating_count': rating_counts}
+df_product = pd.DataFrame.from_dict(data)
+
+print(df_product.head())
 
 
 # Exporting the data into csv
