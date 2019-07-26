af421403f76f9e34ea18d7075342a9a97f1ee71f
Tatan Vincent <vintatan@visa.com>
2019-07-26 09:05:18 +0700

    Ready for Deployments

--- Selenium Web Scraping/lazada/smtp_alert.py
+++ Selenium Web Scraping/lazada/smtp_alert.py
@@ -1,21 +1,27 @@
 import smtplib
+import configparser
+import re
 
-gmail_user = '<input your gmail user. No need to use @gmail.com>'
-gmail_password = '<input your gmail password>'
+config = configparser.ConfigParser()
+config.read('email_properties.ini')
+gmail_user = config['EMAIL']['user']
+gmail_password = config['EMAIL']['password']
 
 def send_alert_of_price_reduction(product_title = '',original_price = '',latest_price = ''):
+    # product_title = re.sub('\W+','', product_title)
     sent_from = gmail_user
     to = ['vincentkernn@gmail.com']
-    subject = 'Alert: your product price for %s is reduced' % (product_title)
+    subject = 'Alert: your product price is reduced'
     body = 'Alert for reduce in price from %s to %s' % (original_price,latest_price)
 
-    email_text = """\
+
+    email_text = """
     From: %s
     To: %s
-    Subject: %s
     
     %s
-    """ % (sent_from, ", ".join(to), subject, body)
+    %s
+    """ % (sent_from,to,subject, body)
 
     try:
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
